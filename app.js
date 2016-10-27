fa869927cf15a92db97327a01695997dcb30dd71
Matthew Paz <paz.matthew@me.com>
2016-10-27 18:25:13 -0400

    Update index part two

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const logger = require('morgan');
 const app = express();
 
 const port = process.env.PORT || process.argv[2] || 3000;
+
+app.set('view engine', 'ejs');
+
+app.listen(port, () => console.log('Server is running on', `${port}`));
