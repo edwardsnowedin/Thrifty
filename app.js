ef867d6d1b1059a94a85d30f8accd36aadfb5901
Matthew Paz <paz.matthew@me.com>
2016-10-28 09:55:49 -0400

    Another Update

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+// Import express modules
 const express = require('express');
 const logger = require('morgan');
 
+// Initialize our express sever
 const app = express();
 
+// The port our server will run on
 const port = process.env.PORT || process.argv[2] || 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(logger('dev'));
+app.use(express.static(__dirname + '/public'));
+
+// Tell our application what port to listen to so we can acess our server
 app.listen(port, () => console.log('Server is running on', `${port}`));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    message: 'Test'
+  })
+})
